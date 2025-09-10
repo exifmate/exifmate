b705eea3897987d5dbfecd12958912409df07ad3
Tegan Rauh <onlysayinghi@gmail.com>
2025-09-09 21:51:16 -0400

    Add a fallback if the thumbnail fails to load

--- src/ImageGrid/ImageCard.tsx
+++ src/ImageGrid/ImageCard.tsx
@@ -1,36 +1,50 @@
 import { readFile } from '@tauri-apps/plugin-fs';
 import { useEffect, useState } from 'react';
+import { MdWarning } from 'react-icons/md';
 import type { ImageInfo } from '../core/types';
 
+type ThumbnailState = {
+  state: 'loading' | 'failed';
+} | {
+  state: 'resolved';
+  assetUrl: string;
+}
+
 function ImageCard({ path, filename }: ImageInfo) {
-  const [assetUrl, setAssetUrl] = useState<string>();
+  const [thumbnail, setThumbnail] = useState<ThumbnailState>({ state: 'loading' });
 
   useEffect(() => {
+    setThumbnail({ state: 'loading' });
+
     readFile(path)
       .then((data) => {
-        const url = URL.createObjectURL(new Blob([data.buffer]));
-        setAssetUrl(url);
+        const assetUrl = URL.createObjectURL(new Blob([data.buffer]));
+        setThumbnail({ assetUrl, state: 'resolved' });
       })
       .catch((err) => {
-        // TODO: should probably have a generic image to show it's errored
-        console.error('Failed to load image', err);
+        setThumbnail({ state: 'failed' })
+        console.error('Failed to load thumbnail:', err);
       });
   }, [path]);
 
   return (
     <div className="card card-xs w-56 bg-neutral">
-      {assetUrl ? (
-        <figure>
-          <img
-            src={assetUrl}
-            alt={`${filename} thumbnail`}
-            className="h-56 w-56 object-cover"
-            height={288}
-            width={288}
-          />
-        </figure>
-      ) : (
+      {thumbnail.state === 'loading' ? (
         <div className="skeleton h-56 w-56"></div>
+      ) : (
+        <figure className="h-56 bg-base-200">
+          {thumbnail.state === 'resolved' ? (
+            <img
+              src={thumbnail.assetUrl}
+              alt={`${filename} thumbnail`}
+              className="h-56 object-cover"
+              height={288}
+              width={288}
+            />
+          ) : (
+            <MdWarning size={40} title="Failed to load thumbnail" />
+          )}
+        </figure>
       )}
 
       <div className="card-body">
