3f5c672ad59e48dc17f64242430e111b30cab5e2
Tegan Rauh <onlysayinghi@gmail.com>
2025-10-06 19:27:12 -0400

    Update tests for metadata-handler

--- src/metadata-handler/__specs__/read.spec.ts
+++ src/metadata-handler/__specs__/read.spec.ts
@@ -1,26 +1,57 @@
+import { invoke } from '@tauri-apps/api/core';
+import type { Mock } from 'vitest';
+import { ZodError } from 'zod/v4';
 import type { ExifData } from '../exifdata';
 import { aggregateExif, readMetadata } from '../read';
 
+const mockInvoke = invoke as unknown as Mock<typeof invoke>;
+
+vi.mock('@tauri-apps/api/core');
+
 describe('readMetadata', () => {
-  it.todo('calls a Tauri command to read metadata');
+  it('calls a Tauri command to read metadata', async () => {
+    mockInvoke.mockResolvedValueOnce([]);
+    expect(mockInvoke).not.toHaveBeenCalled();
+    await readMetadata([
+      { path: '/one.jpg', filename: 'image-one' },
+      { path: '/two.jpg', filename: 'image-two' },
+    ]);
+    expect(mockInvoke).toHaveBeenCalledExactlyOnceWith('read_metadata', {
+      imgPaths: ['/one.jpg', '/two.jpg'],
+    });
+  });
+
+  // for some reason this only asserts that the returned is an array
+  // but it drops the invalid object keys, so i don't care to look into
+  it('enforces that the Tauri command returns valid data', async () => {
+    mockInvoke.mockResolvedValueOnce({ not: 'valid' });
+    await expect(async () => {
+      await readMetadata([{ path: '/one.jpg', filename: '' }]);
+    }).rejects.toThrowError(ZodError);
+  });
 
   describe('when reading multiple images', () => {
-    it.todo('aggregates the common metadata of images', async () => {
-      const exif = await readMetadata([
-        { path: '/image-one.jpg', filename: 'image-one' },
-        { path: '/image-two.jpg', filename: 'image-two' },
+    it('aggregates the common metadata of images', async () => {
+      mockInvoke.mockResolvedValueOnce([
+        { Artist: 'Foo', Make: 'Test' },
+        { Artist: 'Bar', Make: 'Test' },
       ]);
 
-      // undefined is treated as different
+      const exif = await readMetadata([
+        { path: '/one.jpg', filename: 'one' },
+        { path: '/two.jpg', filename: 'two' },
+      ]);
       expect(exif).toEqual({ Make: 'Test' });
     });
   });
 
   describe('when exiftool was unsuccessful', () => {
-    it.todo('can notify of that', async () => {
+    it('can notify of that', async () => {
+      mockInvoke.mockRejectedValueOnce('no');
+
       await expect(async () => {
-        await readMetadata([{ path: '/image-one.jpg', filename: '' }]);
-      }).rejects.toThrowError('Failed to read exif data for /image-one.jpg');
+        await readMetadata([{ path: '/one.jpg', filename: '' }]);
+      }).rejects.toThrowError('no');
     });
   });
 });
--- src/metadata-handler/__specs__/update.spec.ts
+++ src/metadata-handler/__specs__/update.spec.ts
@@ -1,11 +1,35 @@
-describe('updateMetadata', () => {
-  it.todo('calls a Tauri command to write metadata');
+import { invoke } from '@tauri-apps/api/core';
+import type { Mock } from 'vitest';
+import { updateMetadata } from '../update';
 
-  describe('when an image fails to save', () => {
-    it.todo('indicates the error');
+const mockInvoke = invoke as unknown as Mock<typeof invoke>;
+
+vi.mock('@tauri-apps/api/core');
+
+describe('updateMetadata', () => {
+  it('calls a Tauri command to write metadata', async () => {
+    expect(mockInvoke).not.toHaveBeenCalled();
+    const newData = { Artist: 'Test' };
+    await updateMetadata(
+      [
+        { path: 'one.jpg', filename: 'one' },
+        { path: 'two.jpg', filename: 'two' },
+      ],
+      newData,
+    );
+    expect(mockInvoke).toHaveBeenCalledExactlyOnceWith('write_metadata', {
+      newData,
+      imgPaths: ['one.jpg', 'two.jpg'],
+    });
   });
 
-  describe('when there is a warning', () => {
-    it.todo('warns of the warning');
+  describe('when an image fails to save', () => {
+    it('indicates the error', async () => {
+      mockInvoke.mockRejectedValueOnce('no');
+
+      await expect(async () => {
+        await updateMetadata([{ path: 'one.jpg', filename: 'one' }], {});
+      }).rejects.toThrowError('no');
+    });
   });
 });
