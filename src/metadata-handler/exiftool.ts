88da71b5412fc046506789e9eba9f66d6d09b3f7
Tegan Rauh <onlysayinghi@gmail.com>
2025-10-13 16:09:51 -0500

    Use settings to build additional flags for exiftool

--- src/metadata-handler/exiftool.ts
+++ src/metadata-handler/exiftool.ts
@@ -1,7 +1,25 @@
+import { loadSettings } from '@app/platform/settings';
 import { Command } from '@tauri-apps/plugin-shell';
 
+async function additionalFlags() {
+  const settings = await loadSettings();
+  const flags: string[] = [];
+
+  if (settings.originalFileBehavior === 'overwrite_original') {
+    flags.push('-overwrite_original');
+  } else if (settings.originalFileBehavior === 'overwrite_original_in_place') {
+    flags.push('-overwrite_original_in_place');
+  }
+
+  return flags;
+}
+
 export async function execute(args: string[]) {
-  const res = await Command.create('exiftool', ['-q', ...args]).execute();
+  const res = await Command.create('exiftool', [
+    '-q',
+    ...(await additionalFlags()),
+    ...args,
+  ]).execute();
 
   if (res.stderr || res.code !== 0) {
     throw new Error(res.stderr);
