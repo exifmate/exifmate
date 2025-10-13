b6c39dfd1503400002597762cf9d80287eb2c6e1
Tegan Rauh <onlysayinghi@gmail.com>
2025-10-12 21:32:39 -0500

    Increase text size in fieldset

--- src/components/Fieldset.tsx
+++ src/components/Fieldset.tsx
@@ -7,7 +7,7 @@ interface Props {
 
 function Fieldset({ legend, children }: Props) {
   return (
-    <fieldset className="fieldset p-4 rounded-box bg-neutral gap-3">
+    <fieldset className="fieldset p-4 rounded-box bg-neutral gap-3 text-sm">
       <legend className="fieldset-legend">{legend}</legend>
       {children}
     </fieldset>
