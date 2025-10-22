298adbbc5d091f8925f310d5e22079d7d0ca545c
Tegan Rauh <onlysayinghi@gmail.com>
2025-10-22 18:37:09 -0400

    Forward logging to terminal for debugging release

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,12 @@
 import { createAppMenu } from '@platform/app-menu';
+import { forwardLogging } from '@platform/logging';
 import SettingsModal from '@screens/SettingsModal/SettingsModal';
 import Shell from '@screens/Shell/Shell';
 import ToastRegion from '@screens/Toasts/ToastRegion';
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 
+forwardLogging();
 const root = document.getElementById('root');
 if (!root) {
   throw new Error('Failed to mount react');
