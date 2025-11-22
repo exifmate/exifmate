c4e2b1bc2048017737b39c1bee7433ee2f617ebe
Tegan Rauh <onlysayinghi@gmail.com>
2025-11-22 16:31:53 -0500

    Fallback image icon

--- src/screens/Shell/ImageGridPanel/ImageCard.tsx
+++ src/screens/Shell/ImageGridPanel/ImageCard.tsx
@@ -1,10 +1,11 @@
 import { Image, Tooltip } from '@heroui/react';
 import type { ImageInfo } from '@platform/file-manager';
 import { readFile } from '@tauri-apps/plugin-fs';
+import { HiOutlineExclamationTriangle } from 'react-icons/hi2';
 import useSWR from 'swr';
 
 function ImageCard({ path, filename }: ImageInfo) {
-  const { data, isLoading } = useSWR(
+  const { data, isLoading, error } = useSWR(
     path,
     (p) =>
       readFile(p).then((data) => URL.createObjectURL(new Blob([data.buffer]))),
@@ -16,18 +17,21 @@ function ImageCard({ path, filename }: ImageInfo) {
     },
   );
 
-  // TODO: Need to get a fallback icon again
   return (
     <Tooltip content={filename} placement="bottom" color="secondary" showArrow>
-      <figure>
-        <Image
-          src={data}
-          isLoading={isLoading}
-          alt={`${filename} thumbnail`}
-          className="h-56 object-cover"
-          height={224}
-          width={224}
-        />
+      <figure className="rounded-large bg-content2 flex justify-center items-center h-56 w-56">
+        {error ? (
+          <HiOutlineExclamationTriangle size={40} />
+        ) : (
+          <Image
+            src={data}
+            isLoading={isLoading}
+            alt={`${filename} thumbnail`}
+            className="h-56 object-cover"
+            height={224}
+            width={224}
+          />
+        )}
         <figcaption className="sr-only">{filename}</figcaption>
       </figure>
     </Tooltip>
