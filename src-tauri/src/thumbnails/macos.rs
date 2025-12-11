87ad0ece73890eb6523311590b223473df1b1f12
Tegan Rauh <onlysayinghi@gmail.com>
2025-12-11 01:51:21 -0500

    Pull out what will be common to the mod file

--- src-tauri/src/thumbnails/macos.rs
+++ src-tauri/src/thumbnails/macos.rs
@@ -1,9 +1,4 @@
-use fast_image_resize::{images::Image, IntoImageView, ResizeOptions, Resizer};
-use image::{codecs::png::PngEncoder, DynamicImage, ImageBuffer, ImageEncoder};
-use std::{
-    io::{BufWriter, Cursor},
-    time::Instant,
-};
+use image::{DynamicImage, ImageBuffer, Rgba};
 use swift_rs::{swift, Int, SRData, SRObject, SRString};
 
 #[repr(C)]
@@ -15,44 +10,18 @@ struct PixelBufferResult {
 
 swift!(fn load_image_pixels(path: SRString) -> Option<SRObject<PixelBufferResult>>);
 
-const THUMBNAIL_SIZE: Int = 300;
-
-pub fn load_thumbnail(path: String) -> Result<Vec<u8>, String> {
+pub fn load_image(path: String) -> Result<DynamicImage, String> {
     let Some(pixel_info) = (unsafe { load_image_pixels(path.as_str().into()) }) else {
         return Err("Failed to resize image".to_string());
     };
 
-    let Some(img) = ImageBuffer::<image::Rgba<u8>, Vec<u8>>::from_raw(
+    let Some(img) = ImageBuffer::<Rgba<u8>, Vec<u8>>::from_raw(
         pixel_info.width.try_into().unwrap(),
         pixel_info.height.try_into().unwrap(),
         pixel_info.data.to_vec(),
     ) else {
         return Err("Failed to create image from pixel data".to_string());
     };
-    let dynimg = DynamicImage::ImageRgba8(img);
 
-    let mut dst_image = Image::new(THUMBNAIL_SIZE, THUMBNAIL_SIZE, dynimg.pixel_type().unwrap());
-
-    let mut resizer = Resizer::new();
-    resizer
-        .resize(
-            &dynimg,
-            &mut dst_image,
-            &ResizeOptions::new()
-                .fit_into_destination((0.5, 0.5).into())
-                .resize_alg(fast_image_resize::ResizeAlg::Nearest),
-        )
-        .unwrap();
-
-    let mut result_buf = Cursor::new(Vec::new());
-    PngEncoder::new(&mut result_buf)
-        .write_image(
-            dst_image.buffer(),
-            THUMBNAIL_SIZE,
-            THUMBNAIL_SIZE,
-            dynimg.color().into(),
-        )
-        .unwrap();
-
-    Ok(result_buf.into_inner())
+    Ok(DynamicImage::ImageRgba8(img))
 }
--- src-tauri/src/thumbnails/mod.rs
+++ src-tauri/src/thumbnails/mod.rs
@@ -1,8 +1,12 @@
+use fast_image_resize::{images::Image, IntoImageView, ResizeOptions, Resizer};
+use image::{codecs::png::PngEncoder, ImageEncoder};
+use std::io::Cursor;
+
 #[cfg(target_os = "macos")]
 mod macos;
 
 #[cfg(target_os = "macos")]
-pub use macos::*;
+use macos::load_image;
 
 #[cfg(target_os = "linux")]
 mod linux;
@@ -15,3 +19,34 @@ mod windows;
 
 #[cfg(target_os = "windows")]
 pub use windows::*;
+
+const THUMBNAIL_SIZE: u32 = 300;
+
+#[cfg(target_os = "macos")]
+pub fn load_thumbnail(path: String) -> Result<Vec<u8>, String> {
+    let dynimg = load_image(path)?;
+    let mut dst_image = Image::new(THUMBNAIL_SIZE, THUMBNAIL_SIZE, dynimg.pixel_type().unwrap());
+
+    let mut resizer = Resizer::new();
+    resizer
+        .resize(
+            &dynimg,
+            &mut dst_image,
+            &ResizeOptions::new()
+                .fit_into_destination((0.5, 0.5).into())
+                .resize_alg(fast_image_resize::ResizeAlg::Nearest),
+        )
+        .unwrap();
+
+    let mut result_buf = Cursor::new(Vec::new());
+    PngEncoder::new(&mut result_buf)
+        .write_image(
+            dst_image.buffer(),
+            THUMBNAIL_SIZE,
+            THUMBNAIL_SIZE,
+            dynimg.color().into(),
+        )
+        .unwrap();
+
+    Ok(result_buf.into_inner())
+}
