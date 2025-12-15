e4c38ea8cc22c011a7dfbf89942ae2b4ac60ade4
Tegan Rauh <onlysayinghi@gmail.com>
2025-12-14 23:50:29 -0500

    Better variable names and error handling

--- src-tauri/src/thumbnails/mod.rs
+++ src-tauri/src/thumbnails/mod.rs
@@ -1,4 +1,4 @@
-use fast_image_resize::{images::Image, IntoImageView, ResizeOptions, Resizer};
+use fast_image_resize::{images::Image, IntoImageView, ResizeAlg::Nearest, ResizeOptions, Resizer};
 use image::{codecs::png::PngEncoder, ImageEncoder};
 use std::io::Cursor;
 
@@ -23,29 +23,32 @@ pub use windows::load_image;
 const THUMBNAIL_SIZE: u32 = 300;
 
 pub fn create_thumbnail(path: String) -> Result<Vec<u8>, String> {
-    let dynimg = load_image(path)?;
-    let mut dst_image = Image::new(THUMBNAIL_SIZE, THUMBNAIL_SIZE, dynimg.pixel_type().unwrap());
+    let original_image = load_image(path)?;
+    let Some(original_pixel_type) = original_image.pixel_type() else {
+        return Err("Unable to determine original pixel type".to_string());
+    };
+    let mut thumbnail = Image::new(THUMBNAIL_SIZE, THUMBNAIL_SIZE, original_pixel_type);
 
     let mut resizer = Resizer::new();
     resizer
         .resize(
-            &dynimg,
-            &mut dst_image,
+            &original_image,
+            &mut thumbnail,
             &ResizeOptions::new()
                 .fit_into_destination((0.5, 0.5).into())
-                .resize_alg(fast_image_resize::ResizeAlg::Nearest),
+                .resize_alg(Nearest),
         )
-        .unwrap();
+        .map_err(|_| "Resizing failed".to_string())?;
 
     let mut result_buf = Cursor::new(Vec::new());
     PngEncoder::new(&mut result_buf)
         .write_image(
-            dst_image.buffer(),
+            thumbnail.buffer(),
             THUMBNAIL_SIZE,
             THUMBNAIL_SIZE,
-            dynimg.color().into(),
+            original_image.color().into(),
         )
-        .unwrap();
+        .map_err(|_| "Failed encoding thumbnail to PNG".to_string())?;
 
     Ok(result_buf.into_inner())
 }
