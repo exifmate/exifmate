be978d6e295432c850e32f4b97d644013ac00470
Tegan Rauh <onlysayinghi@gmail.com>
2026-04-27 18:45:03 -0400

    Fix type issue

--- src/screens/Shell/__specs__/Shell.spec.tsx
+++ src/screens/Shell/__specs__/Shell.spec.tsx
@@ -2,6 +2,7 @@ import { readMetadata } from '@metadata-handler/read';
 import { IMAGES_OPENED_EVENT } from '@platform/file-manager';
 import FileMenu, { REVEAL_IN_DIR_EVENT } from '@platform/menus/file-menu';
 import { emit } from '@tauri-apps/api/event';
+import type { MenuItem } from '@tauri-apps/api/menu';
 import { mockIPC } from '@tauri-apps/api/mocks';
 import { revealItemInDir } from '@tauri-apps/plugin-opener';
 import type { load } from '@tauri-apps/plugin-store';
@@ -177,14 +178,14 @@ describe('Shell', () => {
 
     it('enables the reveal in dir menu item', async () => {
       const item = await FileMenu.get('reveal-in-dir');
-      expect(item.setEnabled).toHaveBeenLastCalledWith(true);
+      expect((item as MenuItem).setEnabled).toHaveBeenLastCalledWith(true);
 
       await userEvent.keyboard('{Meta>}');
       await userEvent.click(screen.getByLabelText('image-two.jpg'));
       await userEvent.keyboard('{/Meta}');
 
       await waitFor(() =>
-        expect(item.setEnabled).toHaveBeenLastCalledWith(false),
+        expect((item as MenuItem).setEnabled).toHaveBeenLastCalledWith(false),
       );
     });
   });
